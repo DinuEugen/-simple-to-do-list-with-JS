10e131619c53a91d85903c899c98f730a004dc11
DinuEugen <129606626+DinuEugen@users.noreply.github.com>
2023-11-23 17:46:04 +0100

    first commit

---
+++ style.js
@@ -0,0 +1,24 @@
+const day = "";
+switch (new Date().getDay()) {
+  case 1:
+    console.log("Astazi luni" + day);
+    break;
+  case 2:
+    console.log("Astazi marti " + day);
+    break;
+  case 3:
+    console.log("Astazi miercuri" + day);
+    break;
+  case 4:
+    console.log("Astazi joi" + day);
+    break;
+  case 5:
+    console.log("Astazi vineri" + day);
+    break;
+  case 6:
+    console.log("Astazi sambata" + day);
+    break;
+  case 7:
+    console.log("Astazi duminica" + day);
+    break;
+}
